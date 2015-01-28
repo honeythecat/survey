8994fc92749f1295e3380a9af75f1cc9a0b65aeb
Heather Anderson & Dexter Fitch <student@epicodus.com>
2015-01-28 10:11:21 -0800

    Add frontend.

--- app.rb
+++ app.rb
@@ -1,1 +1,16 @@
+require("bundler/setup")
+Bundler.require(:default)
+
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+post('/surveys') do
+  title = params["title"]
+  survey = Survey.new({:title => title, :id => nil})
+  survey.save()
+  redirect('/')
+end
